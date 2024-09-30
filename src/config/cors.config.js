5b8ffd1689c0101e0ff9d958fce3e4f7fd820dcc
Jonathas <jonathass5678@gmail.com>
2024-09-30 12:23:00 -0300

    techfixfr_server_postgress

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,4 +1,4 @@
-const whitelist = ['https://techfixfr.vercel.app', 'https://techfixfr.vercel.app/*', 'http://localhost:5173/', 'http://localhost:5173/*'];
+const whitelist = ['https://techfixfr.vercel.app','http://localhost:5173'];
 
 const corsOptions = {
   origin: function (origin, callback) {
