d9e6c3d4ecf4e7c8b3ce24ef67d9e8d4e134c422
Jonathas <jonathass5678@gmail.com>
2025-03-27 13:31:00 -0300

    serverTechFix

--- src/connectionBD/connectiondb.js
+++ src/connectionBD/connectiondb.js
@@ -3,7 +3,7 @@ const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
 function createSupabaseClient() {
-  const supabaseUrl = 'https://iuiequgxuvymympgaidf.supabase.co';
+  const supabaseUrl = 'https://jsbjhyjalghwtdgbuxlu.supabase.co';
   const supabaseKey = process.env.SUPABASE_KEY;
   return createClient(supabaseUrl, supabaseKey);
 }
